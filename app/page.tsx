2ab10b27cc8a31f7d54ce2f6d18b13489aec5d36
ForteHealthcare <carol@fortehealthcare.com.au>
2025-03-05 15:11:44 +1100

    Updated Header Name of Page
    Header changed from Calendar app to Forte Sales Calendar

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function CalendarApp() {
           <div className="container flex h-14 items-center">
             <div className="mr-4 flex">
               <a className="mr-6 flex items-center space-x-2" href="/">
-                <span className="font-bold">Calendar App</span>
+                <span className="font-bold">Forte Sales Calendar</span>
               </a>
             </div>
           </div>
